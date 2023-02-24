c81b40af565d07686be621ae24a8530c0b44ee31
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-02-24 22:48:28 +0700

    feat: ssr and not found page

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,44 @@
+import { doc, getDoc } from 'firebase/firestore'
+import { GetServerSidePropsContext } from 'next'
+import React from 'react'
+import safeJsonStringify from 'safe-json-stringify'
+import { Community } from '../../../atoms/communitiesAtom'
+import CommunityNotFound from '../../../components/Community/NotFound'
+import { firestore } from '../../../firebase/clientApp'
+type CommunityPageProps = {
+  communityData: Community
+}
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  if (!communityData) {
+    return <CommunityNotFound />
+  }
+  return <div>Welcome to {communityData.id}</div>
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // get community data and pass it to client
+  try {
+    const communityDocRef = doc(
+      firestore,
+      'communities',
+      context.query.communityId as string
+    )
+    const communityDoc = await getDoc(communityDocRef)
+
+    return {
+      props: {
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data })
+            )
+          : '',
+      },
+    }
+  } catch (error) {
+    // Could add error page here
+    console.log('getServerSideProps Error: ' + error)
+  }
+}
+
+export default CommunityPage
