006ef194c34b95418f009e52d8bcd61b77ed4c08
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-02-26 14:47:47 +0700

    feat: NewPostForm

--- src/components/Community/Submit.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const SubmitPostPage: React.FC = () => {
-  return <div>SubmitPostPage</div>
-}
-export default SubmitPostPage
---
+++ src/components/Posts/NewPostForm.tsx
@@ -0,0 +1,93 @@
+import { Flex, Icon } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { BiPoll } from 'react-icons/bi'
+import { BsLink45Deg, BsMic } from 'react-icons/bs'
+import { IoDocumentText, IoImageOutline } from 'react-icons/io5'
+import TextInputs from './PostForm/TextInputs'
+import TabItem from './TabItem'
+
+type NewPostFormProps = {}
+
+const formTabs: TabItemType[] = [
+  {
+    title: 'Post',
+    icon: IoDocumentText,
+  },
+  {
+    title: 'Images & Video',
+    icon: IoImageOutline,
+  },
+  {
+    title: 'Link',
+    icon: BsLink45Deg,
+  },
+  {
+    title: 'Poll',
+    icon: BiPoll,
+  },
+  {
+    title: 'Talk',
+    icon: BsMic,
+  },
+]
+
+export type TabItemType = {
+  title: string
+  icon: typeof Icon.arguments
+}
+
+const NewPostForm: React.FC<NewPostFormProps> = () => {
+  const [selectedTab, setSelectedTab] = useState(formTabs[0].title)
+  const [textInputs, setTextInputs] = useState({
+    title: '',
+    body: '',
+  })
+  const [selectedFile, setSelectedFile] = useState<string>()
+  const [loading, setLoading] = useState(false)
+
+  const handleCreatePost = async () => {}
+
+  const onSelectImage = () => {}
+
+  const onTextChange = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const {
+      target: { name, value },
+    } = event
+
+    setTextInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
+  return (
+    <Flex direction='column' bg='white' borderRadius={4} mt={2}>
+      <Flex width='100%'>
+        {formTabs.map((item) => (
+          <TabItem
+            key={item.title}
+            item={item}
+            selected={item.title === selectedTab}
+            setSelectedTab={setSelectedTab}
+          />
+        ))}
+      </Flex>
+      <Flex p={4}>
+        {selectedTab === 'Post' && (
+          <TextInputs
+            textInputs={textInputs}
+            handleCreatePost={handleCreatePost}
+            onChange={onTextChange}
+            loading={loading}
+          />
+        )}
+      </Flex>
+    </Flex>
+  )
+}
+
+export default NewPostForm
